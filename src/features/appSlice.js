7d0c8016cb11ccd8230c3707b3df1989a6369b5f
Ashish Sah <ashishsah173@gmail.com>
2021-11-29 09:32:32 +0530

    Initial Commit

---
+++ src/features/appSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: null,
+};
+
+export const appSlice = createSlice({
+    name: "app",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload;
+        },
+        logout: (state) => {
+            state.user = null;
+        },
+    },
+});
+
+export const { login, logout } = appSlice.actions;
+
+export const selectUser = (state) => state.app.user;
+
+export default appSlice.reducer;
